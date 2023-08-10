64874c5d1784851331548bc41af59340ec65fee9
Hanler <hmelnitzkij.danil85@gmail.com>
2023-08-11 00:22:47 +0200

    settings: Add the settings file with a dict of the proj config

---
+++ settings.py
@@ -0,0 +1,8 @@
+settings = {
+    "temp_on": 45,      # the temperature of turning on
+    "temp_off": 40,     # the temperature of turning off
+    "control_pin": 14,  # the signal pin
+    "sleep_time": 1,    # time to sleep after the iteration
+    "log_freq": 5,      # the log frequency (in iterations)
+    "log_dir": "./logs/"    # the dir for logs
+}
