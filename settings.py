4f980acbd622b24d1cc0da46b8ca1a2932e8eb31
Hanler <hmelnitzkij.danil85@gmail.com>
2023-08-12 16:48:02 +0200

    settings: Change the turn off\on temp

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 settings = {
-    "temp_on": 45,      # the temperature of turning on
-    "temp_off": 40,     # the temperature of turning off
+    "temp_on": 55,      # the temperature of turning on
+    "temp_off": 45,     # the temperature of turning off
     "control_pin": 14,  # the signal pin
     "sleep_time": 1,    # time to sleep after the iteration
     "log_freq": 5,      # the log frequency (in iterations)
